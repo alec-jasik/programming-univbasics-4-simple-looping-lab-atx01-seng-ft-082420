530c69a79a4bc92fd4185a7133436f21a2a9e650
Alec Jasik <alec_jasik@yahoo.com>
2020-08-04 21:03:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,23 @@
-# Write your methods here
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
+
+def loop_message_n_times(message, n)
+  count = 0 
+  while count < n do 
+    puts message 
+    count += 1 
+  end 
+end
+
+def output_array(array)
+  count = 0 
+  while array[0] do
+  puts array 
+end
+end
+  
